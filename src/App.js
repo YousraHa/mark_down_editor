635b710a5b74791bb3341f27477079b5ecea8b2e
unknown <yousrah02@hotmail.com>
2021-06-01 19:58:28 +0200

    last version

--- src/App.js
+++ src/App.js
@@ -22,13 +22,8 @@ class App extends Component {
   componentDidUpdate(){
     const {text} = this.state;
     localStorage.setItem('text', text)
-  }
+  };
 
-  handleShow = () => {
-    const isShow = !this.state.isShow;
-    this.setState({ isShow });
-    console.log('isshow', isShow)
-  }
 
   handleChange = event => {
     const text = event.target.value;
@@ -38,7 +33,8 @@ class App extends Component {
   renderText = text =>{
     const __html = marked(text, {sanitize: true})
     return {__html}
-}
+  };
+  
   render(){
     return (
       <div className='container'>
@@ -46,12 +42,8 @@ class App extends Component {
           <div className="col-sm-6">
             <textarea onChange={this.handleChange} value={this.state.text} className="form-control" rows="35"/>
           </div>
-            <button onClick={this.handleShow}>Boutton</button>
           <div className="col-sm-6">
             <div dangerouslySetInnerHTML={this.renderText(this.state.text)}></div>
-            <div>
-              {sampleText}
-            </div>
           </div>
         </div>
       </div>
@@ -60,44 +52,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-// import React, {Component} from 'react';
-// import './App.css';
-// import marked from 'marked';
-// import {sampleText} from './sampleText';
-
-// class App extends Component {
-//   state={
-//     text: sampleText
-//   };
-
-//   handleChange = event =>{
-//     const text = event.target.value;
-//     this.setState({text})
-//   };
-
-//   renderText = text =>{
-//     const __html = marked(text, { sanitize : true });
-//     return {__html}
-//   }
-
- 
-//   render(){
-//     return(
-//       <div className="container">
-//         <div className="row">
-//           <div className="col-sm-6">
-//             <textarea value={this.state.text} onChange={this.handleChange} rows="35"/>
-//           </div>
-//           <div className="col-sm-6">
-//             <div dangerouslySetInnerHTML={this.renderText(this.state.text)} >
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// export default App;
---
+++ src/Membre.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+
+const Membre = ({nom, famille }) => {
+    return (
+        <Fragment>
+            <h1>Membre : {nom}</h1>
+            {/* <p>{chi²ldren}</p> */}
+        </Fragment>
+    )
+}
+
+export default Membre
